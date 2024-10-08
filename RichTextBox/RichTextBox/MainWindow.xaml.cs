3fbbb59d61f516d6a76af1f83cd1bb686ed3f903
Viktor080181 <161635347+Viktor080181@users.noreply.github.com>
2024-10-08 20:43:23 +0300

    Add files via upload

---
+++ RichTextBox/RichTextBox/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System.IO;
+using System.Windows;
+using System.Windows.Documents;
+
+namespace RichTextBox
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void LoadDocumentToRtb(string filePath)
+        {
+            TextRange documentText = new TextRange(DocumentRtb.Document.ContentStart, DocumentRtb.Document.ContentEnd);
+
+            if (File.Exists(filePath))
+            {
+                using (FileStream fs = new FileStream(filePath, FileMode.Open))
+                {
+                    documentText.Load(fs, DataFormats.Text);
+                }
+            }
+            else
+            {
+                documentText.Text = String.Empty;
+                MessageBox.Show("Такого файла не существует.");
+            }
+        }
+
+        private void OpenFile(object sender, RoutedEventArgs e)
+        {
+            this.LoadDocumentToRtb(FilePathTb.Text);
+        }
+
+        private void DocumentRtb_DragEvent(object sender, DragEventArgs e)
+        {
+            TextRange documentText = new TextRange(DocumentRtb.Document.ContentStart, DocumentRtb.Document.ContentEnd);
+            string fileName = (e.Data.GetData(DataFormats.FileDrop) as string[])[0];
+            this.LoadDocumentToRtb(fileName);
+        }
+    }
+}
